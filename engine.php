a57b1d3d1596e89b4947f7a798471457c2ba918e
anar123 <anar199510@gmail.com>
2015-09-07 10:21:04 +0900

    zasav

--- engine.php
+++ engine.php
@@ -1,6 +1,6 @@
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Tooni mashin</title>
+<title>Зээлийн тооцоолуур</title>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta http-equiv="Content-Style-Type" content="text/css">
@@ -8,108 +8,147 @@
         <meta name="CocoaVersion" content="1347.57">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
             <style type="text/css">
+                table {
+                                
+                                background: url(.jpg) no-repeat;
+                                background-position:left;
+                                background-color:#eff5fb;
+
+                            }
+
+                body {
+                                background-repeat: no-repeat;
+                                background-position: center;
+
+                                margin-right: 50px;
+                                margin-left: 50px
+                                
+                            } 
+                .table1{
+                                margin-top:50px;
+                                background:font-size:20px;
+                                
+                            } 
+                
+                
             </style>
+                        
     </head>
-<body>
-
-<div class="row">
-<div class="col-md-7"  align="center" >
-<img class="pic" src="ARD.jpg" align="left">
-</div>
-<div class="col-md-5"  align="center" >
-<table class="table" flames="void" rules="none">
-<tr><td><div class="h3" align="center">About us</td>
-<td><div class="h3" align="center">Information</td>
-<td><div class="h3" align="center">Contact</td>
-<td><div class="h3" align="center"></td>
-<td><div class="h3" align="center"></td>
-<td><div class="h3" align="center"></td>
-</tr>
-</table>
-</div>
-</div>
-
-<div class="row">
-<div class="col-md-3"  >
-
-
-
-
-
-
-
-<div id="main">
-<div class="container">
-<div class="row">
-<div class="col-sm-3 sidebar">
-<div class="subscribe">
-<h1 class="title"><i class="fa fa-calculator"></i>Зээлийн <br>тооцоолуур</h1>
-<form action="engine.php" method="GET">
-<span>Зээлийн хэмжээ:</span>
-<input type="number" name="loanamount"  class="form-control"  placeholder="Зээлийн хэмжээ" 
-value="<?php if($_GET{"loanamount"}!=0) echo $_GET{"loanamount"}; ?>">
-<br>
-<span>Зээлийн хүү (%):</span>
-<input type="number" name="interestrate" class="form-control" placeholder="Жилийн хүү(%)" value="<?php if( $_GET{"interestrate"}!=0) echo $_GET{"interestrate"}; ?>"  />
-<br>
-<span>Хугацаа:</span>
-<input type="number" name="length" class="form-control" placeholder="Сараар" value="<?php if($_GET{"length"}!=0) echo  $_GET{"length"}; ?>"/>
-<br>
-<input type="submit" value="Тооцоолох" class="btn btn-lg"  placeholder="Зээл төлөлтийн хугацаа (сараар)"/>
-</form>
-</div>
-</div>
-
-<br>
-<div class="col-md-9"  >
-<br>
-<br>
-<br>
-<br>
+    <body>
+            <div class="row">
+                 <div class="col-md-4"  align="center" >
+                      <img class="pic" src="ARD.jpg" align="left">
+                </div>
+                <div class="col-md-2"  align="center" >
+                </div>
+                <div class="col-md-6" >
+                    <table class="table1" width="100%" rules="cols">
+                        <div class="container-fluid">
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                              <ul class="nav navbar-nav">
+                              <tr>
+                                <td><li>
+                                  <a href="http://ardassets.com/about">Бидний тухай</a>
+                                </li></td>  
+                                <td><li>
+                                  <a href="http://ardassets.com/pension">Бүтээгдэхүүн</a>
+                                </li></td>
+                                <td><li>
+                                  <a href="http://ardassets.com/category/news">Мэдээлэл</a>
+                                </li></td>
+                                <td><li>
+                                  <a href="http://ardassets.com/contact">Холбоо барих</a>
+                                </li></td></tr>
+                              </ul>
+                            </div>
+                        </div>
+                </table>
+            </div>
+            </div>
 
 
-<table border="1" width="100%" class="form-control">
+            <div class="row">
+               
+                    
+                    <div class="col-md-4">
+                        <div class="subscribe">
+                            <h1 class="title"><i class="fa fa-calculator"></i>Зээлийн <br>тооцоолуур</h1>
+                            <form action="engine.php" method="GET">
+                                <span>Зээлийн хэмжээ:</span>
+                                <input type="number" name="loanamount"  class="form-control"  placeholder="Зээлийн хэмжээ" 
+                                    value="<?php if($_GET{"loanamount"}!=0) echo $_GET{"loanamount"}; ?>">
+                                <br>
+                                <span>Зээлийн хүү (%):</span>
+                                <input type="number" name="interestrate" class="form-control" placeholder="Жилийн хүү(%)" value="<?php if( $_GET{"interestrate"}!=0) echo $_GET{"interestrate"}; ?>"  />
+                                <br>
+                                <span>Хугацаа:</span>
+                                <input type="number" name="length" class="form-control" placeholder="Сараар" value="<?php if($_GET{"length"}!=0) echo  $_GET{"length"}; ?>"/>
+                                <br>
+                                <input type="submit" value="Тооцоолох" class="btn btn-lg"  placeholder="Зээл төлөлтийн хугацаа (сараар)"/>
+                            </form>
+                        </div>
+                    </div>
+                    <br>                       
+                    <div class="col-md-8" >    
+                    <div class="table-responsive">
+                        <table border="1"  class="form-control" align="center">
+                           <?php
+                    
 
-<?php
-    
+                            if(isset($_GET["loanamount"])&&isset($_GET["interestrate"])&&isset($_GET["length"])){
 
-    if(isset($_GET["loanamount"])&&isset($_GET["interestrate"])&&isset($_GET["length"])){
+                            $P=$_GET{"loanamount"};
+                            $r=$_GET{"interestrate"}/100/12;	//neg sarin huu (butarhai toogoor)
+                            $l=$_GET["length"];
+                            $tmp=pow(1+$r,$l);
+                            $A=$P*$r*$tmp/($tmp-1);  //$P -> zeelin hemje  $A -> neg sard tuluh tulbur  $A=$huugiin+$undsen
+                            
+                           
+                            
 
-        $P=$_GET{"loanamount"};
-        $r=$_GET{"interestrate"}/100/12;	//neg sarin huu (butarhai toogoor)
-        $l=$_GET["length"];
-        $tmp=pow(1+$r,$l);
-        $A=$P*$r*$tmp/($tmp-1);  //$P -> zeelin hemje  $A -> neg sard tuluh tulbur  $A=$huugiin+$undsen
-       
-        
-
-        
-        echo "<tr><th>Сар</th>";
-        echo "<th>Y3T</th>";
-        echo "<th>XT</th>";
-        echo "<th>TT</th>";
-        echo "<th>Үндсэн зээлийн үлдэгдэл</th></tr>";
-        
-        for($etssiinuldegdel=floatval($P),$time=0;$time<$l;$time++){
-            $huugiin=$etssiinuldegdel*$r;
-            $sariinuldegdel=$A-$huugiin;
-            $etssiinuldegdel-=$sariinuldegdel;
-            $Aladin=$time+1;
-            
-            
-            echo "<tr><td>".$Aladin."</td>";
-            echo "<td>" .number_format($sariinuldegdel,2)."</td>";
-            echo "<td>" .number_format($huugiin,2)."</td>";
-            echo "<td>" .number_format($A,2). "</td>";
-            echo "<td>" .number_format($etssiinuldegdel,2)."</td></tr>";
-            
-        }
-    }
-    ?>
-</table>
-</div>
+                                
+                                    
+                                        echo "<tr><th> Сард төлөх зээлийн хэмжээ </th>";
+                                        echo "<th> Хугацаа </th></tr>";
+                                        echo "<td>" .number_format($A,2). "</td>";
+                                        echo "<td>" .$l. "</td>";
+                                   
+                             
 
 
+                            ?>
+                        </table>
+                            <br>
+                            <br>
+                            <br>
+                                <table border="1" width="100%" class="form-control">
+                                    <?php
 
-</body>
+                                    echo "<tr><th>Сар</th>";
+                                    echo "<th>Үндсэн зээлийн төлөлт</th>";
+                                    echo "<th>Хүүгийн төлөлт</th>";
+                                    echo "<th>Тэнцүү төлөлт</th>";
+                                    echo "<th>Үндсэн зээлийн үлдэгдэл</th></tr>";
+                                    
+                                    for($etssiinuldegdel=floatval($P),$time=0;$time<$l;$time++){
+                                        $huugiin=$etssiinuldegdel*$r;
+                                        $sariinuldegdel=$A-$huugiin;
+                                        $etssiinuldegdel-=$sariinuldegdel;
+                                        $Aladin=$time+1;
+                                        
+                                        
+                                        echo "<tr><td>".$Aladin."</td>";
+                                        echo "<td>" .number_format($sariinuldegdel,2)."</td>";
+                                        echo "<td>" .number_format($huugiin,2)."</td>";
+                                        echo "<td>" .number_format($A,2). "</td>";
+                                        echo "<td>" .number_format($etssiinuldegdel,2)."</td></tr>";
+                                        
+                                    }
+                                }
+                                ?>
+                            </table>
+                    </div>
+                    </div>   
+            </div>
+    </body>
 </html>
