0afffcc54c8693d250cf1c1006bae6bd6bfc1ded
Egiiegii <erdenebulag.0812@gmail.com>
2015-09-07 17:46:37 +0900

    nemelt hasalt

---
+++ login.php
@@ -0,0 +1,146 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Tooni mashin</title>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <!-- Bootstrap -->
+    <link rel="icon" type="image/png" href="http://ardassets.com/wp-content/themes/assets/images/favicon.png" />
+    <link href="http://ardassets.com/wp-content/themes/assets/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="http://ardassets.com/wp-content/themes/assets/css/swiper.min.css">
+    <link href="http://ardassets.com/wp-content/themes/assets/dist/css/font-awesome.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="http://ardassets.com/wp-content/themes/assets/css/style.css" type="text/css" />
+    <meta name='robots' content='noindex,follow' />
+    <link rel="alternate" type="application/rss+xml" title="ardassets.com &raquo; Зээлийн тооцоолуур Comments Feed" href="http://ardassets.com/calculator/feed/" />
+
+            <style type="text/css">
+
+                table {
+                                
+                                background: url(.jpg) no-repeat;
+                                background-position:left;
+
+                            }
+                
+
+                body {
+                                background-repeat: no-repeat;
+                                background-position: center;
+
+                                margin-right: 50px;
+                                margin-left: 50px;
+                                padding:20px;
+
+                                
+                                
+                            } 
+                .container-fluid{
+
+                                padding:10px; 
+
+                             }
+                .col-md-4{
+                                padding:0px;
+                            }
+                .table1{
+
+                                width:100%;
+
+                             }
+                #main{
+                        background-color: white;
+                        box-shadow: 0px 0px 6px #888;
+                       
+
+                            
+                }
+                
+
+            </style>
+                        
+    </head>
+    <body>  
+
+            <div class="row" id="main">
+                <div class="col-md-4"  align="center" >
+                      <img class="pic" src="ARD.jpg" align="left" height="75" width="200">
+                </div>
+                <div class="col-md-2"  align="center" >
+                </div>
+                <div class="col-md-6" >
+                    <background-repeat>
+                    <table   width="100%" rules="cols"  >
+                        <div class="container-fluid">
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                              <ul class="nav navbar-nav">
+                              <tr>
+                                <td align="center" >
+                                  <a href="http://ardassets.com/about"><font color="#848484">Бидний тухай</font></a>
+                                </td >  
+                                <td align="center">
+                                  <a href="http://ardassets.com/pension"><font color="#848484">Бүтээгдэхүүн</font></a>
+                                </td>
+                                <td align="center">
+                                  <a href="http://ardassets.com/category/news"><font color="#848484">Мэдээлэл</font></a>
+                                </td>
+                                <td align="center">           
+                                   <a href="http://ardassets.com/contact"><font color="#848484">Холбоо барих</font></a>
+                                </td>
+                              </tr>
+                              </ul>
+                            </div>
+                        </div>
+                    </table>
+                </div>
+            </div>
+
+            <br>
+            <br>
+
+            <div class="row">   
+                    <div class="col-md-4">
+                        <form action="loginservlet.php" method="POST">
+                                <span>Name:</span>
+                                <input type="text" name="username"  class="form-control">
+                                <br>
+                                <span>Password:</span>
+                                <input type="text" name="userpassword" class="form-control">
+                                <br>
+                                <input type="submit" value="Нэвтрэх" class="btn btn-lg"  placeholder="Зээл төлөлтийн хугацаа (сараар)"/>
+
+                            </form>
+                    </div>
+                    </div>   
+            </div>
+            <div id="side-btn">
+                <a href="http://ardassets.com/online">
+                    Онлайн зээлийн<br>ѳргѳдѳл
+                </a>
+            </div>
+            <div id="footer">
+                <div class="container">
+                    <div class="pull-left">Ард Актив © 2015 Бүх эрх хуулиар хамгаалагдсан.
+                        <div>
+                              <ul class="social pull-right">
+                                      <li class="fa fa-facebook">
+                            <a href="http://www.facebook.com/ardassetscom" target="_blank">
+                              facebook
+                            </a>
+                          </li>
+                                                  <li class="fa fa-twitter">
+                            <a href="http://twitter.com/ardassets" target="_blank">
+                              twitter
+                            </a>
+                          </li>
+                                                  <li class="fa fa-linkedin">
+                            <a href="http://www.linkedin.com/company/ard-assets" target="_blank">
+                              linkedin
+                            </a>
+                          </li>
+                                  </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </body>
+</html>
